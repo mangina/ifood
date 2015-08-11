67cf904cd31544fab7d74308c35ecf7ff8a48d80
mangina <mangina.yulya@gmail.com>
2015-08-11 17:17:56 +0500

    change index.html, style.css, styleIE8, iphone-gallary.js

--- js/iphone-gallary.js
+++ js/iphone-gallary.js
@@ -1,10 +1,26 @@
-imgs = [
-    'img/Nav-bar---Green.jpg',
-    'img/Nav-bar---Cuisines.jpg',
-    'img/Nav-bar---Benefits.jpg',
-];
+var numberImage = 4, i = 0, opacity = 100, timeout;
 
-setInterval(function(){
-    img = imgs[Math.floor(Math.random()*imgs.length)];
-    $('#iphone__gallary').attr('src', img);
-}, 5000);
+function nextImage() {
+    opacity--;
+    var k = i + 1;
+    var image_now = 'image_' + i;
+    if (i == numberImage)
+        k = 1;
+    var image_next = 'image_' + k;
+    document.getElementById(image_now).style.opacity = opacity/100;
+    document.getElementById(image_now).style.filter = 'alpha(opacity='+ opacity +')';
+    document.getElementById(image_next).style.opacity = (100-opacity)/100;
+    document.getElementById(image_next).style.filter = 'alpha(opacity='+ (100-opacity) +')';
+    timeout = setTimeout("nextImage()",1);
+    if (opacity==1) {
+        opacity = 100;
+        clearTimeout(timeout);
+    }
+}
+setInterval (
+    function() {
+        i++;
+        if (i > numberImage) i=1;
+            nextImage();
+    }, 1000
+);
